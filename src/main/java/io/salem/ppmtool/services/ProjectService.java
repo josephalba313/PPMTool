d7c5863c7d4d65dc1c12d6d7075bfabe51a9eaf5
josephalba313 <josephalba313@gmail.com>
2019-05-06 12:10:09 +0800

    Custom Exceptions for Unique Project

--- src/main/java/io/salem/ppmtool/services/ProjectService.java
+++ src/main/java/io/salem/ppmtool/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.salem.ppmtool.domain.Project;
+import io.salem.ppmtool.exceptions.ProjectIdException;
 import io.salem.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -13,7 +14,12 @@ public class ProjectService {
 	private ProjectRepository projectRepository;
 	
 	public Project savedOrUpdateProject(Project project) {
-		return projectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);			
+		} catch (Exception e) {
+			throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists." );
+		}
 	}
 
 }
