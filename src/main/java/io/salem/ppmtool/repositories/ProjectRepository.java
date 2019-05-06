ac7b18bbc1954f1a5611e272c32d53bdf210776f
josephalba313 <josephalba313@gmail.com>
2019-05-06 12:27:44 +0800

    Custom Exceptions for Unique Project

--- src/main/java/io/salem/ppmtool/repositories/ProjectRepository.java
+++ src/main/java/io/salem/ppmtool/repositories/ProjectRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import io.salem.ppmtool.domain.Project;
 
 public interface ProjectRepository extends CrudRepository<Project, Long> {
-	Iterable<Project> findAllById(Iterable<Long> ids);
+	Project findByProjectIdentifier(String projectId);
 }
